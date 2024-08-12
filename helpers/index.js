e1d8b3ae9d68309e22a2054b41bf04b72581697e
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-08-12 18:37:43 +0300

    add handleMongooseError helper

--- helpers/index.js
+++ helpers/index.js
@@ -1,7 +1,9 @@
 const ctrlWrapper = require("./controllerWrapper");
 const HttpError = require("./HttpError");
+const handleMongooseError = require("./handleMongooseError");
 
 module.exports = {
   ctrlWrapper,
   HttpError,
+  handleMongooseError,
 };
