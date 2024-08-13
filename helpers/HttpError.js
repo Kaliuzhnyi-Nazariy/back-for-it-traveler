05a24d32a8ef35d8b863c7cc969fb98da8d0039f
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-08-13 16:34:56 +0300

    add an object of errors for shorten

--- helpers/HttpError.js
+++ helpers/HttpError.js
@@ -1,4 +1,12 @@
-const HttpError = (status, message) => {
+const errorsStauses = {
+  400: "Bad request",
+  401: "Unauthorized",
+  403: "Forbidden",
+  404: "Not Found",
+  409: "Conflict",
+};
+
+const HttpError = (status, message = errorsStauses[status]) => {
   const error = new Error(message);
   error.status = status;
   return error;
