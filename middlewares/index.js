75afb0095be6e43f7b5defe832c5ecd7c30ee398
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-08-13 16:35:19 +0300

    add authenticated middleware

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,7 +1,9 @@
 const validateBody = require("./validateBody");
 const isValidId = require("./isValidId");
+const authenticated = require("./authenticated");
 
 module.exports = {
   validateBody,
   isValidId,
+  authenticated,
 };
