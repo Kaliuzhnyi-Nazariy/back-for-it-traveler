821368bee91170b6407c8ce294af7962c030a49f
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-08-21 21:12:09 +0300

    add to joi schema default value

--- models/place.js
+++ models/place.js
@@ -31,7 +31,7 @@ const addSchema = Joi.object({
   location: Joi.string().min(3).required(),
   description: Joi.string().min(15).required(),
   coordinates: Joi.array().required(),
-  img: Joi.string().optional(),
+  img: Joi.string().default("../public/placePhotos/no-image.png").optional(),
 });
 
 const schemas = { addSchema };
